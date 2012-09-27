d775ff656f61fe46a8d931b3181125d0a5e8f7c5
ralamosm <ralamosm@gmail.com>
2012-09-27 20:04:57 +0000

    setup changes

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
+import os
 from distribute_setup import use_setuptools; use_setuptools()
 from setuptools import setup, find_packages
+
+rel_file = lambda *args: os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
+cleanup = lambda lines: filter(None, map(lambda s: s.strip(), lines))
+
+def read_from(filename):
+    fp = open(filename)
+    try:
+        return fp.read()
+    finally:
+        fp.close()
+
+def get_requirements():
+    return cleanup(read_from(rel_file('REQUIREMENTS')).splitlines())
 
 setup(
     name="readability-lxml",
@@ -12,15 +26,12 @@ setup(
     long_description= "Given a html, it pulls out the main body text",
     license="Apache License 2.0",
     url="http://github.com/ralamosm/python-readability",
-    packages=['readability'],
-    install_requires=[
-        "chardet",
-        "lxml"
-        ],
+    packages=find_packages(),
+    install_requires = get_requirements(),
     classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        ],
+        ]
 )
